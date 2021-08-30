6b92379363907266952f8a588b4a077983d3bf69
Chris Pereira <89607691+Nabozny@users.noreply.github.com>
2021-08-30 14:05:03 -0300

    Clean
    Script to clean every pc at my work.

---
+++ Script.ps1
@@ -0,0 +1,29 @@
+﻿Write-Output "Hello World"
+
+{
+    Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullName Downloads) -Recurse} | %{Remove-Item $_.FullName -Verbose}
+    Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullName Documents) -Recurse} | %{Remove-Item $_.FullName -Verbose}
+    Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullName Music) -Recurse} | %{Remove-Item $_.FullName -Verbose}
+    Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullName Videos) -Recurse} | %{Remove-Item $_.FullName -Verbose}
+    Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullName Pictures) -Recurse} | %{Remove-Item $_.FullName -Verbose}
+    Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullName Desktop) -Recurse} | %{Remove-Item $_.FullName -Verbose}
+
+}
+
+
+  {
+   
+    del C:\Users\mobile\AppData\Local\Temp\*.*
+    del C:\Documents and Settings\Local Settings\Temporary Internet Files\*.*
+    del C:\Documents and Settings\Local Settings\Temp\*.*
+    del C:\Users\AppData\Local\Temp\*.*
+    del C:\Users\AppData\Local\Microsoft\Windows\Temporary Internet Files\*.*
+    del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\History"
+   
+  }
+     {
+
+        del 'C:\$Recycle.Bin'
+
+     } 
+
