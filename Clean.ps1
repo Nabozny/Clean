0743bd4e7a0d466696b8b9c233ec441aff7022bd
Chris Pereira <89607691+Nabozny@users.noreply.github.com>
2023-05-29 11:57:28 -0300

    Update Clean.ps1

--- Clean.ps1
+++ Clean.ps1
@@ -11,24 +11,7 @@ Get-ChildItem "C:\Users" | ?{ $_.PSIsContainer }  | % {gci (Join-Path $_.FullNam
  
  del C:\Windows\Temp\*.*;
  del "C:\ProgramData\Package Cache\*.*";
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\History" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Login Data" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Cookies" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Login Data For Account" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\LOG" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Preferences" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Network Action Predictor" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Shortcuts" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\LOG.old" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Top Sites" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\LOCK" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Favicons" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Trust Tokens" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Web Data" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Quota Manager" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Reporting and NEL" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\PreferedApps" ;
- del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\Visited Links" ;
+ del "C:\Users\mobile\AppData\Local\Google\Chrome\User Data\Default\*.*;
  del C:\Users\mobile\AppData\Local\Temp\*.* 
 
  del 'C:\$Recycle.Bin';
